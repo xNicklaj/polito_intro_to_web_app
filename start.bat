6dbf4efcf55c8b40cf5fc7de048b1109ddd549f2
Ravn <simonearonica@protonmail.ch>
2023-01-09 13:14:13 +0100

    Changed init scripts to start scripts

---
+++ start.bat
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+set FLASK_APP=app\__init__.py
+flask run --host=0.0.0.0
--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 pip3 install -r requirements.txt;
-cd /app/db
-sqlite3 App.db ".read init.sql"
-cd ../static
-mkdir audio
-mkdir images
-mkdir tmp
-cd ../../
 export FLASK_APP=app/__init__.py && flask run --host=0.0.0.0
