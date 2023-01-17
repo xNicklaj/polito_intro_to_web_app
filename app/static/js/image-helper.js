8003f97705e489cb36ef155b28648ce7432f76a5
Ravn <simonearonica@protonmail.ch>
2023-01-17 21:59:09 +0100

    Added image preview to the new podcast page

---
+++ app/static/js/image-helper.js
@@ -0,0 +1,27 @@
+// This small piece of code loads default podcast image when an error occurs
+// in the src tag of an image
+document.querySelectorAll('img').forEach(i => {
+    i.addEventListener('error', (e) => e.target.src = '/static/images/pod_default.jpg')
+})
+
+// This function allows to preview the image that will be sent when creating a new podcast
+function previewFile(target, src){
+    var reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      target.src = reader.result;
+    }
+  
+    if (src) {
+      reader.readAsDataURL(src);
+    } else {
+      target.src = "";
+    }
+}
+ì
+const newpod_thumbnail_input = document.querySelector('#newpod_thumbnail_input');
+const newpod_thumbnail = document.querySelector('#newpod_thumbnail');
+
+if(newpod_thumbnail != null && newpod_thumbnail_input != null){
+    newpod_thumbnail_input.addEventListener('change', () => previewFile(newpod_thumbnail, newpod_thumbnail_input.files[0]))
+}
